9fcb16c403669cfbac9bc07193983acc5b31e402
prathameshpanse51 <93512551+prathameshpanse51@users.noreply.github.com>
2024-05-09 13:06:12 +0530

    Update Footer.jsx

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,7 +4,7 @@ import CopyrightIcon from "@mui/icons-material/Copyright";
 export default function Footer() {
   return (
     <>
-      <footer className="w-[95%] md:w-[98%] h-[100%] flex flex-col md:flex-row gap-10 md:gap-0 justify-between p-[30px] md:p-[60px] mx-auto my-8 bg-[#F5F5F5] rounded-[40px]">
+      <footer className="w-[95%] md:w-[98%] h-[100%] flex flex-col md:flex-row items-center gap-10 md:gap-0 justify-between p-[30px] md:p-[60px] mx-auto my-8 bg-[#F5F5F5] rounded-[40px]">
         <div className="flex flex-row items-center gap-2">
           <CopyrightIcon />{" "}
           <p className="manrope500 text-[#1C1C1C] text-base md:text-lg">
